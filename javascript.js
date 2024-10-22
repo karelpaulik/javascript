017448b0b29e35d4b9af81cf2225337ad7b8adb1
karelpaulik <60463516+karelpaulik@users.noreply.github.com>
2024-10-22 10:32:45 +0200

    Create javascript.js

---
+++ javascript.js
@@ -0,0 +1,17 @@
+DATATYPE CONVERSION
+
+string to number:
+................
+Number("10")
+
+number to string
+................
+String(123)
+nebo
+(123).toString
+
+---------------------------------------------
+DATE
+
+const d = new Date();	//current date and time
+const d = new Date('2024-12-30');
