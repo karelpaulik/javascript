c4381b0dd0adce67b600c190e42649082c58aa45
karelpaulik <60463516+karelpaulik@users.noreply.github.com>
2024-11-05 09:17:41 +0100

    Update jsGeneral.js

--- jsGeneral.js
+++ jsGeneral.js
@@ -38,6 +38,10 @@ JAVASCRIPT - basic DOM operations: HTML, CSS
 document.getElementById("demo").innerHTML = "Hello JavaScript"; 
 document.getElementById("demo").style.fontSize = "35px"; 
 
+const par = document.createElement("p");
+par.innerHTML="asdf";
+document.body.appendChild(par);
+
 JAVASCRIPT - ARITHMETIC
 ........................
 Operator 	Description
@@ -49,13 +53,26 @@ Operator 	Description
 % 	Modulus (Remainder)
 ++ 	Increment	//let x=1; x++;
 -- 	Decrement
-
 Math.sqrt(81)	//9
+
+JAVASCRIPT - setTimeout (akce jednou), setInterval (akce opakovaně)
+....................................
+setTimeout(()=>console.log("aaa"), 1000)
+
+let t = 0;
+setInterval(()=>{
+    t++;
+    console.log(t);
+}, 1000)
+
+
+
+
 
 JAVASCRIPT - date operations
 ............................
     const d = new Date();					//Current date and time
-    //const d = new Date('2023-05-20');		//Zadání datumu
+	//const d = new Date('2023-05-20');		//Zadání datumu
     console.log(d);							//Date Mon Nov 04 2024 15:36:41 GMT+0100 (středoevropský standardní čas) (objekt typu date)
     console.log(d.toDateString());			//Mon Nov 04 2024
     console.log(d.toISOString());			//2024-11-04T14:36:41.174Z
@@ -67,8 +84,8 @@ JAVASCRIPT - date operations
     console.log(d.toTimeString());			//15:36:41 GMT+0100 (středoevropský standardní čas)
     console.log(d.toUTCString());			//Mon, 04 Nov 2024 14:36:41 GMT
     console.log("----------");				
-    console.log(d.getFullYear());			//2024
-    console.log(d.getMonth());				//10		//month 0-11
+	console.log(d.getFullYear());			//2024
+	console.log(d.getMonth());				//10		//month 0-11
     console.log(d.getDate());				//4		//day of the month (1 to 31)
     console.log(d.getDay());				//1		//day of the week (0 to 6) 
     console.log(d.getHours());
@@ -79,3 +96,28 @@ JAVASCRIPT - date operations
     d.setFullYear(2020);	
     d.setMonth(3);
     //d.set...								//analogicky dále
+	
+JAVASCRIPT - event handler. Two possible ways.
+..............................................
+click, mouseover, mouseout, keydown, load, ...
+
+keydown - Specific key:
+    document.addEventListener("keydown", (event) => {
+        if (event.key === "Enter") {
+            console.log("Enter key was pressed!");
+        }
+    });
+
+<button id="btn">Klik</button>
+<script>
+    const btn = document.getElementById("btn");
+	
+    //first option - not the best way (only one event handler)
+    btn.onclick=()=>console.log("click1");  //Not done. This property is overwriten. 
+    btn.onclick=()=>console.log("click2");  //Just this is done
+	
+    //second option - best way (Multipe event handler allowed)
+    btn.addEventListener("click", ()=> console.log("click3"));  //Both done
+    btn.addEventListener("click", ()=> console.log("click4"));  //Both done
+</script>
+
