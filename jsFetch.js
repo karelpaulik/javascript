b208fd40fd4a941bfeb3bc386bd512454f922449
karelpaulik <60463516+karelpaulik@users.noreply.github.com>
2024-11-08 15:35:21 +0100

    Create jsFetch.js

---
+++ jsFetch.js
@@ -0,0 +1,4 @@
+Chrom potřebuje pro "fetch" lokálního souboru povolit CORS.
+
+To se prování při spouštění chromu přepínači:
+"C:\Program Files\Google\Chrome\Application\chrome.exe" --disable-web-security --user-data-dir="D:\temp2\pokus"
