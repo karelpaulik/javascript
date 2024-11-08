65737e1fafe9a4545a29422c5c64116fad2d09c1
karelpaulik <60463516+karelpaulik@users.noreply.github.com>
2024-11-08 15:41:33 +0100

    Update jsFetch.js

--- jsFetch.js
+++ jsFetch.js
@@ -1,4 +1,21 @@
+JAVASCRIPT - fetch
+..........................................................................................
 Chrom potřebuje pro "fetch" lokálního souboru povolit CORS.
 
 To se prování při spouštění chromu přepínači:
 "C:\Program Files\Google\Chrome\Application\chrome.exe" --disable-web-security --user-data-dir="D:\temp2\pokus"
+...........................................................................................
+1.
+
+fetch("pokus.txt")
+.then(x => x.text())
+.then(y => console.log(y));
+............................................................................................
+2.
+f1("pokus.txt");
+
+async function f1(file) {
+let x = await fetch(file);
+let y = await x.text();
+console.log(y);
+}
