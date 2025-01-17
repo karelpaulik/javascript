dfaea8197914840d8ec24a3f8f1aa59bdfb80c7d
karelpaulik <60463516+karelpaulik@users.noreply.github.com>
2025-01-17 11:24:51 +0100

    Add files via upload

---
+++ msSQL_responseAsJSON.sql
@@ -0,0 +1,43 @@
+FOR JSON PATH vs FOR JSON AUTO
+PATH - more control, more work
+AUTO - simpler
+
+Note - ROOT (for PATH and AUTO as Well): Example: FOR JSON AUTO, ROOT('root_object')
+--///////////////////////////////////////////////////////////
+FOR JSON PATH
+
+SELECT 
+    id_kalk AS 'id_kalk',
+    part_nr AS 'part_nr',
+    razeni AS 'Tab_kalk_str_bom.razeni',
+    level_bom AS 'Tab_kalk_str_bom.level_bom'
+FROM MainTable
+FOR JSON PATH;
+
+[
+  {
+    "id_kalk": 1587,
+    "part_nr": "10920033",
+    "Tab_kalk_str_bom": {
+      "razeni": 11,
+      "level_bom": "1"
+    }
+  }
+]
+--//////////////////////////////////////////////////////////////////////
+SELECT 
+    id_kalk,
+    part_nr,
+    razeni,
+    level_bom
+FROM MainTable
+FOR JSON AUTO;
+
+[
+  {
+    "id_kalk": 1587,
+    "part_nr": "10920033",
+    "razeni": 11,
+    "level_bom": "1"
+  }
+]
