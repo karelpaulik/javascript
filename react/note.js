9495a52326a656bd8dc95afb32ae3c38041777e3
karelpaulik <60463516+karelpaulik@users.noreply.github.com>
2025-01-20 15:23:07 +0100

    Update note.js

--- react/note.js
+++ react/note.js
@@ -1,7 +1,8 @@
 How to use react
-----------------
+//---------------------------------------------------------------------------------------
 // 1. Directly in the html file
 // just for testin
+// Source: w3schools
 ...........................
 <!DOCTYPE html>
 <html>
@@ -26,10 +27,38 @@ How to use react
 
   </body>
 </html>
-//----------------------------------------------
+//---------------------------------------------------------------------------------------------
 // 2. create-react-app
+// Toto je způsob, který byl doporučován dříve. Není nyní ani na "react.dev" stránkách.
 // This procedure is since 2023 deprecated
+// Source: w3schools, https://www.itnetwork.cz
 npx create-react-app app  //Tímto se vytvoří aplikace, při instalaci ty vyhodí nějaké chyby
 cd app
 npm install web-vitals
 npm start
+
+Tímto by se měl spustit prohlížeč, ve kterém poběží nová react aplikace.
+//---------------------------------------------------------------------------------------------
+// 3. Frameworky (nuxt.js, Remix, Gatsby, Expo)
+
+// 3a. nuxt - automatic installation
+// Použiji: nuxt.js
+npx create-next-app@latest
+
+What is your project named? my-app              //Project name
+Would you like to use TypeScript? No / Yes      //No
+Would you like to use ESLint? No / Yes          //No
+Would you like to use Tailwind CSS? No / Yes    //No
+Would you like your code inside a `src/` directory? No / Yes                //No
+Would you like to use App Router? (recommended) No / Yes                    //Yes
+Would you like to use Turbopack for `next dev`?  No / Yes                   //No
+Would you like to customize the import alias (`@/*` by default)? No / Yes   //
+What import alias would you like configured? @/*                            //
+
+// 3b. nuxt - manual installation
+// Nevytváří adresáře: app, ...
+// Nepoužívám
+- md app
+- cd app
+- npm init --yes
+- npm install next@latest react@latest react-dom@latest
